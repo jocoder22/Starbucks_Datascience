2a622035cd5944d1bf58b8e4fbdaeb3ea610d60f
Okechukwu Okigbo <okigbookey@gmail.com>
2020-04-20 09:22:59 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,1 +1,16 @@
-
+Cross-Industry Standard Process of Data Mining - (CRISP-DM)
+A. Bussiness Understanding                 
+    1. Formulate research/analysis question(s)
+B. Data Understanding
+    1. Seek for relevant datasets
+    2. Download relevant datasets
+C. Data preparation
+    1. Clean datasets
+    2. Features extraction and engineering
+    3. Exploratory data analysis
+    4. Data visualization
+D. Modeling
+    1. Supervised learning
+E. Deployment
+    1. Summary report
+    2. Conclusion(s)

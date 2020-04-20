7ce141011cc9a651b03c82b2295f77ab5974fb7d
Okechukwu Okigbo <okigbookey@gmail.com>
2020-04-20 09:38:35 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,19 +1,23 @@
-Cross-Industry Standard Process of Data Mining - (CRISP-DM)
-A. Bussiness Understanding                 
-    1. Formulate research/analysis question(s)
-B. Data Understanding
-    1. Seek for relevant datasets
-    2. Download relevant datasets
-C. Data preparation
-    1. Clean datasets
-    2. Features extraction and engineering
-    3. Exploratory data analysis
-    4. Data visualization
-D. Modeling
-    1. Supervised learning
-E. Deployment
-    1. Summary report
-    2. Conclusion(s)
+# Cross-Industry Standard Process of Data Mining - (CRISP-DM)
+# A. Bussiness Understanding                 
+#     1. Formulate research/analysis question(s)
+    
+#            Question I
+#                 Which demographic groups respond best to which offer type?
+#                         Analyzing the response rate of each gender and age groups.
+#             Question II
+#                 Who is the typical Starbucks rewards mobile app user?
+#                         Analysis of descriptive statistics of all variables in the dataset.
+#             Question III
+#                 Who will response to an offer?
+#                         Build a model that predicts whether or not someone will respond to an offer.
+
+
+
+
+# B. Data Understanding
+#     1. Seek for relevant datasets
+#     2. Download relevant datasets
     
     
 #   import modules and libraries
@@ -23,10 +27,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import datetime as dt
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer    
+    
+# C. Data preparation
+#     1. Clean datasets
+#     2. Features extraction and engineering
+#     3. Exploratory data analysis
+#     4. Data visualization
+# D. Modeling
+#     1. Supervised learning
+# E. Deployment
+#     1. Summary report
+#     2. Conclusion(s)
+    
+    
 
 
-# A. Bussiness Understanding                 
-#     1. Formulate research/analysis question(s)
 
 
